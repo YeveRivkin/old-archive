55b72c6767e6f0702ff4876daf722e7d441635fc
Yeve Rivkin <yeve.rivkin@gmail.com>
2020-03-13 21:00:20 +0200

    initial commit

---
+++ js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+
+    (function($) {
+        "use strict";
+
+
+        jQuery.validator.addMethod('answercheck', function(value, element) {
+            return this.optional(element) || /^\bcat\b$/.test(value)
+        }, "type the correct answer -_-");
+
+        // validate contactForm form
+        $(function() {
+            $('#contactForm').validate({
+                rules: {
+                    name: {
+                        required: true,
+                        minlength: 2
+                    },
+                    subject: {
+                        required: true,
+                        minlength: 4
+                    },
+                    number: {
+                        required: true,
+                        minlength: 5
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    message: {
+                        required: true,
+                        minlength: 20
+                    }
+                },
+                messages: {
+                    name: {
+                        required: "הכנס את שמך",
+                        minlength: "שם חייב להכיל לפחות 2 תווים"
+                    },
+                    subject: {
+                        required: "הכנס נושא",
+                        minlength: "נושא חייב להכיל לפחות 4 תווים"
+                    },
+                    number: {
+                        required: "come on, you have a number, don't you?",
+                        minlength: "your Number must consist of at least 5 characters"
+                    },
+                    email: {
+                        required: "הכנס אימייל"
+                    },
+                    message: {
+                        required: "כתוב משהו כדי שתוכל לשלוח הודעה זו",
+                        minlength: "זה הכל? באמת?"
+                    }
+                },
+                submitHandler: function(form) {
+                    $(form).ajaxSubmit({
+                        type: "POST",
+                        data: $(form).serialize(),
+                        url: "contact_process.php",
+                        success: function() {
+                            $('#contactForm :input').attr('disabled', 'disabled');
+                            $('#contactForm').fadeTo("slow", 1, function() {
+                                $(this).find(':input').attr('disabled', 'disabled');
+                                $(this).find('label').css('cursor', 'default');
+                                $('#success').fadeIn()
+                                $('.modal').modal('hide');
+                                $('#success').modal('show');
+                            })
+                        },
+                        error: function() {
+                            $('#contactForm').fadeTo("slow", 1, function() {
+                                $('#error').fadeIn()
+                                $('.modal').modal('hide');
+                                $('#error').modal('show');
+                            })
+                        }
+                    })
+                }
+            })
+        })
+
+    })(jQuery)
+})
